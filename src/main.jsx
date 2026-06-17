3fe8b5eef9fbc3f6fd8a43171db862bed8ca7ad3
Riley Huntley <rileyhuntleyca@gmail.com>
2026-06-17 00:43:14 +0000

    Shift Type Icons Added

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ThemeProvider } from 'next-themes'
 import App from '@/App.jsx'
 import '@/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ThemeProvider attribute="class" defaultTheme="dark" forcedTheme={null} disableTransitionOnChange={false}>
-    <App />
-  </ThemeProvider>
+  <App />
 )
