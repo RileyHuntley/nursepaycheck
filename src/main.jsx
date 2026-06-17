cd5080b3bb02c1f558b64852da8d6d29be6fbef7
Riley Huntley <rileyhuntleyca@gmail.com>
2026-06-17 00:39:56 +0000

    File changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'next-themes'
 import App from '@/App.jsx'
 import '@/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <ThemeProvider attribute="class" defaultTheme="dark" forcedTheme={null} disableTransitionOnChange={false}>
+    <App />
+  </ThemeProvider>
 )
